a6c49ce8db6dbbbc3e562e5be6ef8930ac91143e
IVÁN_P\Iván <ivan120695@gmail.com>
2018-10-08 18:42:57 +0200

    Fixing hierarchy 2

--- src/HojaEjercicios1.hs
+++
@@ -1,7 +0,0 @@
-module HojaEjercicios1 where
-
--- a) Implementar una función en Haskell que dados tres números enteros determine si están
---    ordenados de menor a mayor. 
-
-ejercicioA :: Int -> Int -> Int -> Bool 
-ejercicioA x y z = if x<y && y<z then True else False
