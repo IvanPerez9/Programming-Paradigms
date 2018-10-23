6ecd67f49ea12711315f21556b7d4259d36ba7b6
IVÁN_P\Iván <ivan120695@gmail.com>
2018-10-23 16:43:10 +0200

    HojaEjerciciosRepasoES 2

--- src/HojasEjercicios/HojaEjerciciosRepasoES.hs
+++ src/HojasEjercicios/HojaEjerciciosRepasoES.hs
@@ -9,6 +9,32 @@ Función que dado un listado de nombres lo muestre por pantalla en forma de tabla
 -}
 
 escribeTabla :: [String] -> IO ()
-escribeTabla (x:xs) =   do
-						putStrLn (show n ++ ":" ++ x)
-						escribeTabla xs where n =1
+escribeTabla s =  escribeTablaAux s 1
+						
+escribeTablaAux :: [String] -> Int -> IO ()
+escribeTablaAux [] cont = return()
+escribeTablaAux (x:xs) cont = do
+							  print(show cont ++ ":" ++ x)
+							  escribeTablaAux xs (cont+1)
+							  
+
+{-
+Definir una función que sea capaz de leer dos líneas de la entrada estándar y las
+compare, escribiendo una cadena por pantalla que indique si son iguales o no.
+> comparaCadenas
+Introduce la primera linea: linea1
+Introduce la segunda linea: hola
+Las cadenas son diferentes
+-}
+
+comparaCadenas :: [String] -> [String] -> IO ()
+comparaCadenas l1 l2 = compararCadenasAux l1 l2 
+
+compararCadenasAux :: [String] -> [String] -> IO ()
+compararCadenasAux [] _ = print "No son iguales" 
+compararCadenasAux _ [] = print "No son iguales"
+compararCadenasAux  
+
+
+
+
