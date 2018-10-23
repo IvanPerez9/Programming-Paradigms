1fe1d83c3152a64f98c3f018d05ec53d5011f031
IVÁN_P\Iván <ivan120695@gmail.com>
2018-10-23 18:48:01 +0200

    Repaso Hoja 1

--- src/HojasEjercicios/RepasoHoja1.hs
+++ src/HojasEjercicios/RepasoHoja1.hs
@@ -1,31 +1,26 @@
-module HojasEjercicios.HojaEjercicios1 where
+module HojasEjercicios.RepasoHoja1 where
 
 import Data.Char
 
 -- a) Implementar una función en Haskell que dados tres números enteros determine si están
 --    ordenados de menor a mayor. ejercicioA 3 4 5 
 
-ejercicioA :: Int -> Int -> Int -> Bool 
-ejercicioA x y z = if (x<y) && (y<z) then True else False
+ejercicioA :: Int -> Int -> Int -> Bool
+ejercicioA x y z = x<y && y>z
 
 -- b) Implementar una función en Haskell que dados tres números enteros los devuelva
---    ordenados de menor a mayor. 
+--    ordenados de menor a mayor
 
 ejercicioB :: Int -> Int -> Int -> (Int,Int,Int)
-ejercicioB x y z  
-				| z>x && z>y && y>x = (x,y,z)
-				| y>x && y>z && z>x = (y,z,x)
-				| x>y && x>z && y>z = (z,y,x)
-				| z>x && z>y && x>y = (z,x,y)
-				| y>x && y>z && z<x = (y,x,z)
-				| x>y && x>z && z>y = (y,z,x)
-			    | otherwise =(1,1,1)
-			  
+ejercicioB x y z = if (x>y) then ejercicioB y x z
+					else if (y>z) then ejercicioB x z y
+					else (x,y,z)
+
 -- c)  Implementar en Haskell una función que reciba un número real y devuelva una tupla
 --     con su parte entera y sus dos primeros decimales (como número entero).
 
-ejercicioC :: Double -> (Int,Int)
-ejercicioC x = (truncate(x) , truncate( (x - fromInteger(truncate (x))) * 100))
+ejercicioC :: Double -> (Int , Int)
+ejercicioC num = (truncate num, truncate (num - fromInteger (truncate num))* 100)
 
 -- d) Crear una función que reciba el radio de una circunferencia y devuelva una 2-tupla con
 --  la longitud de la circunferencia y con el área del círculo. Emplea una definición local con
@@ -33,27 +28,23 @@ ejercicioC x = (truncate(x) , truncate( (x - fromInteger(truncate (x))) * 100))
 -- predefinida pi). A continuación crear una función con el mismo cometido empleando la
 -- definición local let.
 
-ejercicioD :: Double -> (Double,Double)
-ejercicioD x = (pi*x*x , pi * x*x) where pi = 3.14
+ejercicioD :: Double -> (Double ,Double)
+ejercicioD radio = (2*pi*radio,pi*radio*radio) where pi = 3.14
 
 ejercicioD' :: Double -> (Double,Double)
-ejercicioD' x = let pi =3.14 in (pi*x*x, pi*x*x)
+ejercicioD' radio = let pi = 3.14 in (2*pi*radio,pi*radio*radio)
 
 -- e) Implementar la función predefinida de listas concat, que se llamará concatenar,
 -- utilizando la definición de listas por comprensión (no se puede utilizar recursividad).
 
 ejercicioE :: [[Int]] -> [Int]
-ejercicioE l = [x | x'<-l , x<-x']
-
--- Derecha va mas rapido que izq
--- x elementos l final
--- x' cada micro lista
+ejercicioE l = [x | y <- l , x <- y]
 
 -- f) Implementar una función que dado un número entero devuelva en una lista todos los
 -- factores de dicho número. Se debe utilizar la definición de listas por comprensión. 
 
 ejercicioF :: Int -> [Int]
-ejercicioF x = [ y | y <- [1..x] , x `mod` y == 0]
+ejercicioF num = [ y | y <- [1..num] , num `mod` y == 0]
 
 {-
  g) Implementar una funcion que diga si un numero es primo. Para ello se debe
@@ -67,35 +58,42 @@ ejercicioG num = length (ejercicioF num) < 3
 -- en una frase dada. Se deberá utilizar la definición de listas por comprensión. 
 
 ejercicioH :: String -> Int
-ejercicioH s = length [c | c <- s, isUpper c]
+ejercicioH s = length [x | x <- s , isUpper x]
 
 -- i) Implementar una función que dada una tupla de tres elementos, donde cada uno de
 -- ellos es a su vez una tupla de dos elementos de tipo String e Int respectivamente,
 --retorne el primer elemento de cada tupla interna. Se deberá utilizar ajuste de patrones. 
 
 ejercicioI :: ((String,Int),(String,Int),(String,Int)) -> (String,String,String)
-ejercicioI ((s,_),(d,_),(f,_)) = (s,d,f)
+ejercicioI ((s,_),(t,_),(k,_)) = (s,t,k)
 
 -- j)  Implementar una función que devuelve True si la suma de los cuatro primeros
 -- elementos de una lista de números enteros es un valor menor a 10 y devolverá False
 -- en caso contrario. Se deberá utilizar ajuste de patrones.
 
 ejercicioJ :: [Int] -> Bool
-ejercicioJ [x,y,z,t] = if (x+y+z+t) < 10 then True else False
-ejercicioJ [] = False
+ejercicioJ [x,y,z,t] = (x+y+z+t) > 10
 
 -- k) Implementar una función que dado un carácter, que representa un punto cardinal,
 -- devuelva su descripción. Por ejemplo, dado ‘N’ devuelva “Norte”.
 
-
+{-
 ejercicioK :: Char -> String
-ejercicioK 'N' = "Norte"
-ejercicioK 'S' = "Sur"
-ejercicioK 'E' = "Este"
-ejercicioK 'O' = "Oeste"
-ejercicioK ' ' = "Mal."
-			
-			
+ejercicioK c = 
+			  |'N' = "Norte"
+			  |'S' = "Sur" 
+			  |'E' = "Este"
+			  |'O' = "Oeste" 
+			  |otherwise "MAL"
+-}		  
+ejercicioK' :: Char -> String
+ejercicioK' 'N' = "Norte"
+ejercicioK' 'S' = "Sur"
+ejercicioK' 'E' = "Este"
+ejercicioK' 'O' = "Oeste"
+ejercicioK' ' ' = "Mal."
+
+
 -- l) Implementar una función que dada una frase retorne un mensaje donde se indique cuál
 -- es la primera y última letra de la frase original. Un ejemplo de aplicación de la función
 -- podría ser
@@ -108,8 +106,7 @@ ultima letra es 'e'"
 -}
 
 ejercicioL :: String -> String
-ejercicioL "" = "Cadena Vacia"
-ejercicioL s = "La primera letra de la frase" ++ s ++ "es" ++ [head s] ++ "Y la ultima es: " ++ [last s]
+ejercicioL frase = "La primera letra es: " ++ [head frase] ++ "La ultima frase es: " ++ [last frase] 
 
 
 -- m) Implementar una función que dado un número entero devuelva mensajes indicando en
@@ -117,16 +114,14 @@ ejercicioL s = "La primera letra de la frase" ++ s ++ "es" ++ [head s] ++ "Y la 
 -- de 20). Se debe utilizar definiciones locales.
 
 ejercicioM :: Int -> String
-ejercicioM x
-			| x< 10 = cad ++ "menor de 10"
-			| x<= 20 = cad ++ "entre 10 y 20"
-			| x >20 = cad ++ "mayor de 20"
-			where cad = "El valor de entrada es "
-			
+ejercicioM num 
+				| num < 10 = "Es menor que 10" 
+				| num < 20 = "Esta entre 10 y 20" 
+				| num > 20 = "Es mayor de 20" 
+				
 -- n) Implementar una función que dada una cadena de caracteres y un carácter, indique el
 -- número de apariciones del carácter en la cadena. No se debe utilizar recursividad, sí
 -- ajuste de patrones. Pista: utilizar la definición de listas por comprensión.
 
-ejercicioN :: String -> Char -> Int
-ejercicioN ""_ = 0
-ejercicioN s c = length [t | t <- s, t == c]
+ejercicioN:: String -> Char -> Int
+ejercicioN s c = length [x| x<-s , c == x ]
