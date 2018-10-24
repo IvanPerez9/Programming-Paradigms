5170e6a9a4184653f3c834877ac3f41ee50f4540
IVÁN_P\Iván <ivan120695@gmail.com>
2018-10-24 18:32:40 +0200

    Ejercicios repasoES 5 y 6

--- src/HojasEjercicios/HojaEjerciciosRepasoES.hs
+++ src/HojasEjercicios/HojaEjerciciosRepasoES.hs
@@ -33,8 +33,73 @@ comparaCadenas l1 l2 = compararCadenasAux l1 l2
 compararCadenasAux :: [String] -> [String] -> IO ()
 compararCadenasAux [] _ = print "No son iguales" 
 compararCadenasAux _ [] = print "No son iguales"
-compararCadenasAux  
+--compararCadenasAux  
 
+{- 3
+Definir una función que lea el contenido de un fichero de texto, lo procese
+invirtiendo todo el contenido y lo escriba de nuevo sobre el mismo fichero de
+entrada. (u otro)
+-}
+invertir :: [a] -> [a]
+invertir = foldr (\x acum -> acum ++ [x]) []
 
+main :: IO ()
+main = do
+		print "Nombre del archivo origen"
+		archivoO <- getLine
+		print "Nombre de larchivo destino"
+		archivoD <- getLine
+		contenido <- readFile archivoO
+		let invertido = foldr (\c acum->acum ++[c])[] contenido
+		writeFile archivoD invertido
+		
+		
+{- 4
+ Definir una función que sea capaz de ir leyendo líneas de la entrada estándar y las
+va imprimiendo junto con el número de caracteres que tienen. Se irá ejecutando
+mientras no se encuentra una línea vacía. Un ejemplo del resultado de la ejecución
+de la función puede ser:
+> leerLineas
+Introduce una linea: hola
+La linea tiene 4 caracteres
+Introduce una linea: casita
+La linea tiene 6 caracteres
+Introduce una linea:
+-}
 
+numeroLista :: IO()
+numeroLista = do -- Meter más de una cosa, lo compacta
+				print "Introduce una linea: "
+				linea <- getLine
+				if linea == [] then 
+					return() -- Devuelve lo que haya arriba
+					else do
+					print ("Tiene: " ++ (show(length linea)) ++ "caracteres" ) 
+					-- Meto 'show' porque length es tipo int y lo otro string
+					numeroLista
+				
 
+-- 5. Definir una función que sea capaz de copiar el contenido de un fichero en otro. 
+
+copiarFichero :: IO ()
+copiarFichero = do
+				print "Nombre del fichero de entrada"
+				archivoO <- getLine
+				print "Nombre archivo de salida"
+				archivoS <- getLine
+				contenido <- readFile archivoO
+				writeFile archivoS contenido 
+				
+-- 6. Calculadora dos digitos y un operador 
+
+calculadora :: IO()
+calculadora = do 
+			  print "El primer digito es: "
+			  num1 <- readLn -- Por se un numero
+			  print "El segundo digito es: "
+			  num2 <- readLn
+			  print "La operacion que se realiza es: (+) (-) (/) (*) "
+			  operacion <- getLine
+			  if operacion == " " then return() 
+			  	else if (operacion == "+") then print(show(num1 + num2) )
+			  	else return()
