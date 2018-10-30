94e03efe0620ed282ec7f97ed80f469f023f83f5
IVÁN_P\Iván <ivan120695@gmail.com>
2018-10-30 15:39:48 +0100

    Python

---
+++ Python/PrimerosEjercicios/Prueba.py
@@ -0,0 +1,17 @@
+'''
+Created on 30 oct. 2018
+
+@author: Iván
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
