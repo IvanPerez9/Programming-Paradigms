0da47052a06a14da2a09b1ebed9d6bfeb1a627a1
Iván Pérez Huete <ivan120695@gmail.com>
2018-11-01 12:48:05 +0100

    Funciones 1 en Python

--- Python/Funciones/Funciones.py
+++ Python/Funciones/Funciones.py
@@ -28,8 +28,39 @@ rombo(n)
 
 def area (base,altura):
     areaA = base*altura
-    print("El area es: " + areaA)
+    print("El area es: " + str(areaA))
     
 base = int(input("Introduces una base: "))
 altura = int(input("Introduces una altura: "))
 area(base, altura)
+
+#Crear una funcion que devuelva el perimetro de una circunferencia (utilizando math).
+
+def perimetro (radio):
+    perime = 2 * math.pi * math.sqrt(radio)
+    print("El perimetro es: " + str(perime))
+
+radio = float (input("Introduce el radio de la circunferencia: "))
+perimetro(radio)
+
+# Crear una funcion que calcule el factorial de n
+
+def factorial (n):
+    if n == 0 :
+        return  1
+    else:
+        return n * factorial(n-1)
+
+n = int(input("Introduzca el valor n para factorial: "))
+print(factorial(n))
+
+# Crear una funcion que resuelve una ecuacion de segundo grado recibiendo a, b, c
+
+def ecuacionSegundo (a,b,c):
+    ecuacion = (-b + math.sqrt(b**2 - (4 * a *c))) / (2*a)
+    print(str(ecuacion))
+
+a = int (input("Introduce el valor de a: "))
+b = int (input("Introduce el valor de b: "))
+c = int (input("Introduce el valor de c: "))
+ecuacionSegundo(a, b, c)
