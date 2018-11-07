1e92f2c9cdff6426a3928fe5e87680ccc5b23129
IVÁN_P\Iván <ivan120695@gmail.com>
2018-11-07 12:47:42 +0100

    Fix aeropuerto

--- Haskell/src/HojaEjercicios/Areopuerto.hs
+++ Haskell/src/HojaEjercicios/Areopuerto.hs
@@ -19,8 +19,8 @@ type Compania = String
 data Aeropuerto = A Nombre Pistas Trafico Accidente [Compania]
 
 instance Show Aeropuerto where
-	show (A n p t acc com) = "Aeropuerto: " ++ n ++ "Pistas: " ++ show p ++ "Trafico: " ++ show t ++ 
-							"Accidente: " ++ show acc ++ "Companias" ++ show (length com)
+	show (A n p t acc com) = "Aeropuerto: " ++ n ++ " Pistas: " ++ show p ++ " Trafico: " ++ show t ++ 
+							" Accidente: " ++ show acc ++ " Companias " ++ show (length com)
 						
 instance Eq Aeropuerto where
 	(A n p t acc com) == (A n2 p2 t2 acc2 com2) = (p==p2) && (t == t2) && ((length com) == (length com2))
@@ -33,4 +33,5 @@ dameLista a (l:ls) = if (a==l) && ((dameAccidentes a) < (dameAccidentes l)) then
 dameAccidentes :: Aeropuerto -> Int
 dameAccidentes  (A n1 p1 t1 a1 l1) = a1
 
+--dameLista (A "Aero1" 3 5 2 ["Pene"] ) [(A "Aero2" 3 5 5 ["Pene"] ),(A "Aero3" 4 5 5 ["Pene"] )]
 
