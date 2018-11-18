0969f9af7de03c36621568632cc34f82e046e3a3
Iván Pérez Huete <ivan120695@gmail.com>
2018-11-18 11:35:28 +0100

    Slice apuntes

--- Python/Repaso/RepasoSet.py
+++ Python/Repaso/RepasoSet.py
@@ -186,4 +186,14 @@ a[:end]      # items from the beginning through end-1
 a[:]         # a copy of the whole array
 
 a[start:end:step] # start through not past end, by step
+
+a[-1]    # last item in the array
+a[-2:]   # last two items in the array
+a[:-2]   # everything except the last two items
+
+a[::-1]    # all items in the array, reversed
+a[1::-1]   # the first two items, reversed
+a[:-3:-1]  # the last two items, reversed
+a[-3::-1]  # everything except the last two items, reversed
+
 """
