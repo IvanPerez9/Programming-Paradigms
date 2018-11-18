713727517b4e810bedf88c8abfe7da07df4b5d14
Iván Pérez Huete <ivan120695@gmail.com>
2018-11-18 11:34:46 +0100

    ForceSet Repaso

--- Python/Repaso/RepasoSet.py
+++ Python/Repaso/RepasoSet.py
@@ -3,7 +3,7 @@ Created on 18 nov. 2018
 
 @author: ivan
 '''
-from _overlapped import NULL
+
 
 """
 
@@ -69,7 +69,7 @@ def cubifinito (num, aux = set()):
     else:
         return False
     
-print(cubifinito(1243, []))
+print(cubifinito(10, []))
          
     
 
@@ -105,3 +105,85 @@ def copia (c1):
         c.add(e)
     return c
 
+
+"""
+
+Los forcesets son set inmutables. Comparacion con issubset() e issuperset() , como poner < > 
+
+"""
+
+# Le paso una lista de parejas de positivos y negativos, todos tienen pareja menos 1 , encontrar y devolver cual es
+
+
+def opuestos (lista):
+    suma = 0
+    for e in lista:
+        suma += e
+    return suma
+
+print(opuestos([1,-1,2,3,4,-3,-2]))
+
+    
+def repetidos (lista):
+    vistos = set()
+    for e in lista:
+        if e not in vistos:
+            vistos.add(e)
+        elif e in vistos:
+            vistos.remove(e)
+    
+    return vistos
+
+print(repetidos([1,2,3,4,2,3,1]))
+
+"""
+Lo mismo pero con un XOR , es un OR pero eliminando repetidos. Ver tabla de verdad
+"""
+
+def single (l):
+    s = 0
+    for e in l:
+        s ^= e # ^ XOR 
+    return s
+
+print(single([1,2,3,4,2,3,1]))
+
+
+def tuplaPares (conjunto):
+    pares = set()
+    impares= set()
+    for e in conjunto:
+        if e % 2 == 0:
+            pares.add(e)
+        else:
+            impares.add(e)
+    return pares,impares # Ojo como devolver la tupla
+
+print(tuplaPares([1,2,3,4,5,6,7,8]))
+
+#Conjunto potencia. Es devolver la lista con subconjuntos de como se creo el conjunto dado
+
+def addTo(e, t):       
+    for s in t:
+        s += [e]
+    return t
+ 
+def conjunto_potencia(a_set):
+        if not a_set: return [[]]
+        e = a_set[0]
+        t = a_set[1:] #Todos menos el primero
+        return conjunto_potencia(t) + addTo(e, conjunto_potencia(t))
+    
+print(conjunto_potencia([1,2,3,4,5]))
+
+"""
+SLICE
+
+
+a[start:end] # items start through end-1
+a[start:]    # items start through the rest of the array
+a[:end]      # items from the beginning through end-1
+a[:]         # a copy of the whole array
+
+a[start:end:step] # start through not past end, by step
+"""
