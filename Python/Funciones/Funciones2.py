b91aca0e32cb0e306e2e573887261a8765b36c57
Iván Pérez Huete <ivan120695@gmail.com>
2018-11-19 21:21:38 +0100

    Ecuacion

--- Python/Funciones/Funciones2.py
+++ Python/Funciones/Funciones2.py
@@ -19,3 +19,21 @@ a = int (input("Introduce el valor de a: "))
 b = int (input("Introduce el valor de b: "))
 c = int (input("Introduce el valor de c: "))
 ecuacionSegundo(a, b, c)
+
+def ecuacion (a,b,c):
+    x = (b ** 2 - (4 * a * c ))
+    if x < 0:
+        print("No hay solucion")
+    elif x == 0:
+        solucion = (-b)/(2*a)
+        print("Solucion: " + str(solucion))
+    else:
+        solucion1 = (-b + math.sqrt(x)) / (2*a)
+        solucion2 = (-b + math.sqrt(x)) / (2*a)
+        print("Solucion 1: " + str(solucion1));
+        print("Solucion 2: " + str(solucion2));
+                    
+a = int (input("Introduce el valor de a: "))
+b = int (input("Introduce el valor de b: "))
+c = int (input("Introduce el valor de c: "))
+ecuacion(a, b, c)
