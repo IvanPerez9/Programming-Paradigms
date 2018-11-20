d938d5251fd91a62ae9ddcfb1be1127f06f55957
Iván Pérez Huete <ivan120695@gmail.com>
2018-11-20 18:22:47 +0100

    SuperHeroe TEST

--- Python/Clases/SuperHeroe.py
+++ Python/Clases/SuperHeroe.py
@@ -10,10 +10,29 @@ class MyClass(object):
     '''
 
 
-    def __init__(self, nombre, apodos,poderes, nacionalidad, estatura, peso):
+    def __init__(self, nombre, apodo,poder, nacionalidad, estatura, peso):
         '''
         Constructor
         '''
+        self.nombre = nombre
+        self.apodos = {apodo} #FrozenSet ?? 
+        self.poderes = {poder}
+        self.nacionalidad = nacionalidad
+        self.estatura = estatura
+        self.peso = peso
         
     #Funciones de añadir y eliminar apodos, si se me queda vacio tiene que ser el nombre
     # Funcion que me pidan los apodos de un super heroe, pillar toda la lista de apodos y usar pop()
+    
+    def añadirApodo(self, apodo):
+        self.apodos.add(apodo)
+        
+    def eliminarApodo(self,apodo):
+        if not self.apodos:
+            self.apodos = {self.nombre}
+        else:
+            self.apodos.remove(apodo)
+    
+    def apodosSuper (self):
+        self.apodos.pop()
+            
