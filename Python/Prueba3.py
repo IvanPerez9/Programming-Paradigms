25b8ac05876172cfa23e44d06f8a2b7d17dbe91d
IVÁN_P\Iván <ivan120695@gmail.com>
2019-06-10 18:30:58 +0200

    Prueba3

---
+++ Python/Prueba3.py
@@ -0,0 +1,24 @@
+
+# Poder sobrescribir sin afectar al metodo que sobreescribo
+
+class SuperHeroe:
+    def __init__(self, nombre, iterable):
+        self.nombre = nombre
+        self.poderes = []
+        self.__añadirPoderes(iterable)
+
+    def añadirPoderes(self, iterable):
+        for e in iterable:
+            self.poderes.append(e)
+x
+    __añadirPoderes = añadirPoderes
+
+
+class Villano(SuperHeroe):
+    def añadirPoderes(self, poderes, costes):
+        for e in zip(poderes, costes):
+            self.poderes.append(e)
+
+
+v = Villano('Lex Luthor', [('rico', 0), ('listo', 10)])
+print(v.poderes)
